bcef3eed18e5b56393b3b0f20c4bb10e26e59138
noksniang5336-cell <noksniang5336@gmail.com>
2026-06-29 01:44:08 +0000

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext' // Ajuste le chemin si ton dossier est différent
+import { AuthProvider } from './context/AuthContext.jsx' // Ajuste le chemin si ton dossier est différent
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
